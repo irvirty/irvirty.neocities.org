884032a85930a25205af894bfc73ae067cd5b6e7
irvirty <irvirty@gmail.com>
2024-10-18 18:51:06 +0300

    update

---
+++ data/microblog2JsonVar.js
@@ -0,0 +1,35 @@
+
+var microblog2JsonVar = 
+
+[
+    {
+        "id": "3",
+        "text": "New page: Microblog. Website updates page and maybe something else.",
+        "text2": "",
+        "text3": "",
+        "url": "",
+        "tag": "#update #new #page",
+        "time": 1729262589,
+        "rightFooter": ""
+    },
+    {
+        "id": "2",
+        "text": "The address of the pages has been changed, there may be 404 errors.",
+        "text2": "",
+        "text3": "",
+        "url": "",
+        "tag": "#update",
+        "time": 1729262033,
+        "rightFooter": ""
+    },
+    {
+        "id": "1",
+        "text": "test",
+        "text2": "",
+        "text3": "",
+        "url": "https:\/\/example.com\/",
+        "tag": "#test",
+        "time": 1729260404,
+        "rightFooter": ""
+    }
+]
