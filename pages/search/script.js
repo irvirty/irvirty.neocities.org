f39cedfb6fc567e2e2838434396b8f8b88f6805e
irvirty <irvirty@gmail.com>
2024-12-30 03:06:24 +0200

    update

--- pages/search/script.js
+++ pages/search/script.js
@@ -288,6 +288,7 @@ document.getElementById("seachButtons").innerHTML = `${lQButtonPrint}`;
 }
 
 
+
 // Google search result
 /*if (q != ""&&q2 == "goo"){
 
@@ -326,3 +327,18 @@ document.getElementById('searchPage').classList.add('contentCenter');
 /*if (String(location.href).indexOf("/search/") != -1){
 fuMBg("light");
 }*/
+
+
+// hide top header if mobile
+if (conf["confDevice"] == 'mobile'){
+var getclick2 = document.getElementById('form');
+if (getclick2 != null&&document.getElementById('topHeader') != null){
+document.addEventListener('click', function(event) {
+if (getclick2.contains(event.target)) {
+document.getElementById('topHeader').style.display = 'none';
+} else {
+document.getElementById('topHeader').style.display = 'block';
+}
+});
+}
+}
