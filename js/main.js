58ecd0bb376a381362ac51f9a35b1bbc4bbd56bd
irvirty <wdvrx5qyp@mozmail.com>
2025-02-12 17:15:18 +0200

    update

--- js/main.js
+++ js/main.js
@@ -266,7 +266,7 @@ fuMInsertHtml("#footer", 'beforeend', `
 
 <a class="brand inlineBlock padding" style="padding-left: 0;" title="About the site" href="/pages/about/">About</a>
 <a class="brand inlineBlock padding" title="RSS News" href="/rss.xml">RSS</a>
-<a class="brand inlineBlock padding" title="Social Network" href="https://bsky.app/profile/${conf["confUsername"]}.bsky.social">Bluesky</a>
+<a class="brand inlineBlock padding" title="Social Network" href="https://bsky.app/profile/${conf["confUsername"]}.pages.dev">Bluesky</a>
 <!--<a class="brand inlineBlock padding" title="RSS News (Site update)" href="https://neocities.org/site/${conf["confUsername"]}.rss">RSS</a>-->
 <a class="brand inlineBlock padding" title="Another home page" href="https://irvirty.pages.dev/">Other Home</a>
 <a id="fSettings" class="brand inlineBlock padding2" title="Settings" href="/pages/settings/">Settings</a>
