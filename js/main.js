75cb62302c12ab3e0cb866cc856d953cbd5ca3df
irvirty <irvirty@gmail.com>
2025-08-14 13:07:34 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -406,7 +406,7 @@ document.getElementById("footer").innerHTML = `
 <a class="brand inlineBlock padding" style="padding-left: 0;" title="Pages" href="${confD}pages/">Pages</a>
 <a class="brand inlineBlock padding" title="About" href="${confD}pages/about/">About</a>
 <a class="brand inlineBlock padding" title="RSS News" href="/rss.xml">RSS</a>
-<a class="brand inlineBlock padding" title="Other license" href="https://bsky.app/">Bluesky</a>
+<a class="brand inlineBlock padding" title="Other license" href="https://bsky.app/profile/${conf["confUsername"]}.bsky.social">Bluesky</a>
 <!--<a class="brand inlineBlock padding" title="RSS News (Site update)" href="https://neocities.org/site/${conf["confUsername"]}.rss">RSS</a>-->
 <a class="brand inlineBlock padding" title="Another home page" href="https://irvirty.pages.dev/#1">Other Home</a>
 <a id="fSettings" class="brand inlineBlock padding2" title="Settings" href="${confD}pages/settings/">Settings</a>
