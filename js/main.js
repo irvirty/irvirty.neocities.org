72841de3a2cc079889302b45ef279656cfda9820
irvirty <irvirty@gmail.com>
2025-09-15 17:02:21 +0300

    update

--- js/main.js
+++ js/main.js
@@ -397,8 +397,8 @@ document.getElementById("footer").innerHTML = `
 <!--<a class="brand inlineBlock padding" style="padding-left: 0;" title="Pages" href="${confD}pages/">Pages</a>-->
 <a class="brand inlineBlock padding" title="About" href="${confD}pages/about/">About</a>
 <a class="brand inlineBlock padding" title="Social" href="https://bsky.app/profile/${conf["confUsername"]}.pages.dev">Bluesky</a>
-<a class="brand inlineBlock padding" title="RSS News" href="/rss.xml">RSS</a>
-<a class="brand inlineBlock padding" title="RSS News (Site update)" href="https://neocities.org/site/${conf["confUsername"]}.rss">RSS2</a>
+<!--<a class="brand inlineBlock padding" title="RSS News" href="/rss.xml">RSS</a>-->
+<a class="brand inlineBlock padding" title="RSS News (Site update)" href="https://neocities.org/site/${conf["confUsername"]}.rss">RSS</a>
 <a id="fSettings" class="brand inlineBlock padding2" title="Settings" href="${confD}pages/settings/">Settings</a>
 <a id="fPrivacy" class="brand inlineBlock padding" title="Cookie Settings" href="${confD}pages/settings/#confDataCollection">Cookie: ${conf["confDataCollection"]}</a>
 <a class="brand inlineBlock padding" title="Source code (repository)" href="https://github.com/${conf["confUsername"]}/${conf["confWebsiteUrl"]}">Source Code</a>
