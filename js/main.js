e6c8fdafc35f9a8e1f36e8145d5d68e50b5e13e9
irvirty <irvirty@gmail.com>
2025-09-16 06:41:48 +0300

    update

--- js/main.js
+++ js/main.js
@@ -970,6 +970,16 @@ fuMSetTheme(conf["confTheme"]);
 
 
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function fuMRandom(min, max){
+//return Math.round(Math.random() * (max - min) + min);
+const minCeiled = Math.ceil(min);
+const maxFloored = Math.floor(max);
+return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+
+
 // CSS
 // random bg image (background img with random position)
 function fuMBg(com, bgImage){
