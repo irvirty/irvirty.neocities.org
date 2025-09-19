b3c0e4a3e0b9139902b5524303896ed77fb242fa
irvirty <irvirty@gmail.com>
2025-09-19 10:19:57 +0300

    update

--- pages/virtual-world/script.js
+++ pages/virtual-world/script.js
@@ -24,8 +24,8 @@ switch (mode) {
 case 'room.png':
 img = mode;
 buttons = `
-<a class="worldClick" style="top: calc(27% - (var(--worldClickHeight) / 2)); left: calc(55% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="index"></a>
-<a class="worldClick" style="top: calc(61% - (var(--worldClickHeight) / 2)); left: calc(5% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="index"></a>
+<a class="worldClick" style="top: calc(27% - (var(--worldClickHeight) / 2)); left: calc(55% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
+<a class="worldClick" style="top: calc(61% - (var(--worldClickHeight) / 2)); left: calc(5% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
 <a class="worldClick" style="top: calc(52% - (var(--worldClickHeight) / 2)); left: calc(37% - (var(--worldClickHeight) / 2));" target="blank" href="https://www.youtube.com/watch?v=59Q_lhgGANc" title="link"></a>
 `;
 printMyWorld(img, buttons);
@@ -34,7 +34,28 @@ break;
 case 'space.png':
 img = mode;
 buttons = `
-<a class="worldClick" style="top: calc(83% - (var(--worldClickHeight) / 2)); left: calc(57% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="index"></a>
+<a class="worldClick" style="top: calc(83% - (var(--worldClickHeight) / 2)); left: calc(57% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
+<a class="worldClick" style="top: calc(35% - (var(--worldClickHeight) / 2)); left: calc(50% - (var(--worldClickHeight) / 2));" href="./?q=mars.png" title="link"></a>
+<a class="worldClick" style="top: calc(15% - (var(--worldClickHeight) / 2)); left: calc(77% - (var(--worldClickHeight) / 2));" href="./?q=sun.png" title="link"></a>
+`;
+printMyWorld(img, buttons);
+break;
+
+case 'sun.png':
+img = mode;
+buttons = `
+<a class="worldClick" style="top: calc(21% - (var(--worldClickHeight) / 2)); left: calc(96% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
+<a class="worldClick" style="top: calc(80% - (var(--worldClickHeight) / 2)); left: calc(15% - (var(--worldClickHeight) / 2));" href="./?q=space.png" title="link"></a>
+`;
+printMyWorld(img, buttons);
+break;
+
+case 'mars.png':
+img = mode;
+buttons = `
+<a class="worldClick" style="top: calc(18% - (var(--worldClickHeight) / 2)); left: calc(64.6% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
+<a class="worldClick" style="top: calc(15% - (var(--worldClickHeight) / 2)); left: calc(40% - (var(--worldClickHeight) / 2));" href="./?q=space.png" title="link"></a>
+
 `;
 printMyWorld(img, buttons);
 break;
@@ -42,7 +63,7 @@ break;
 case 'tpl.png':
 img = mode;
 buttons = `
-<a class="worldClick" style="top: calc(50% - (var(--worldClickHeight) / 2)); left: calc(50% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="index"></a>
+<a class="worldClick" style="top: calc(50% - (var(--worldClickHeight) / 2)); left: calc(50% - (var(--worldClickHeight) / 2));" href="./?q=index.png" title="link"></a>
 `;
 printMyWorld(img, buttons);
 break;
@@ -51,10 +72,11 @@ default:
 img = "index.png";
 buttons = `
 <a class="worldClick" style="top: calc(27% - (var(--worldClickHeight) / 2)); left: calc(50% - (var(--worldClickHeight) / 2));" href="./?q=space.png" title="space"></a>
-<a class="worldClick" style="top: calc(73% - (var(--worldClickHeight) / 2)); left: calc(52.5% - (var(--worldClickHeight) / 2));" href="./?q=room.png" title="room"></a>
-<a class="worldClick" style="top: calc(75% - (var(--worldClickHeight) / 2)); left: calc(65% - (var(--worldClickHeight) / 2));" href="./?q=room.png" title="room"></a>
+<a class="worldClick" style="top: calc(73% - (var(--worldClickHeight) / 2)); left: calc(52.5% - (var(--worldClickHeight) / 2));" href="./?q=room.png" title="link"></a>
+<a class="worldClick" style="top: calc(75% - (var(--worldClickHeight) / 2)); left: calc(65% - (var(--worldClickHeight) / 2));" href="./?q=room.png" title="link"></a>
+<a class="worldClick" style="top: calc(27% - (var(--worldClickHeight) / 2)); left: calc(83% - (var(--worldClickHeight) / 2));" href="./?q=sun.png" title="link"></a>
 
-<a class="worldClick" style="top: calc(95% - (var(--worldClickHeight) / 2)); left: calc(95% - (var(--worldClickHeight) / 2));" href="./?q=tpl.png" title="tpl"></a>
+<a class="worldClick" style="top: calc(95% - (var(--worldClickHeight) / 2)); left: calc(95% - (var(--worldClickHeight) / 2));" href="./?q=tpl.png" title="link"></a>
 `;
 /*buttons = `
 <a class="worldClick" style="top: calc(65% - (var(--worldClickHeight) / 2)); left: calc(50% - (var(--worldClickHeight) / 2));" href="#" onclick="myWorld('room.png');return false;"></a>
