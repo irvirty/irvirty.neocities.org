8c8f49641adb9c7ee01e43f5ba5b6ef17087553b
irvirty <irvirty@gmail.com>
2026-07-03 17:39:02 +0300

    update

--- js/main.js
+++ js/main.js
@@ -423,9 +423,6 @@ document.getElementById("footer").innerHTML = `
 <a class="yellow inlineBlock padding" title="RSS Site update" href="https://neocities.org/site/${conf["confUsername"]}.rss">RSS</a>
 <span class="op gray">|</span>
 
-<a class="brand inlineBlock padding" title="Social" href="https://social.netdynamics.eu/profile/${conf["confUsername"]}">Friendica</a>
-<span class="op gray">|</span>
-
 <a id="fSettings" class="brand inlineBlock padding" title="Settings" href="${confD}pages/settings/">Settings</a>
 <span class="op gray">|</span>
 
